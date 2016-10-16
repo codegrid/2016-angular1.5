3134103069db6a27457f049e342f0bc160499d76
Takeshi Takatsudo <takazudo@gmail.com>
2016-10-16 11:11:04 +0900

    不要ファイル削除
    使ってないみたいなので削除

--- 1-incremental-search/app.js
+++
@@ -1,9 +0,0 @@
-function MainCtrl($scope) {
-  $scope.people = [
-    { firstName: 'Kazuhito', lastName: 'Hokamura' },
-    { firstName: 'Takeshi',  lastName: 'Takatsudo' },
-    { firstName: 'Akihiro',  lastName: 'Oyamada' },
-    { firstName: 'Kazunori',  lastName: 'Tokuda' },
-    { firstName: 'Yukihisa',  lastName: 'Yamada' },
-  ];
-}
