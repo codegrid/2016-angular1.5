d00dedf2247336e1151d128b67a27ba5b89b4c2e
y-maru <maruyama@pxgrid.com>
2016-11-04 10:41:33 +0900

    7回目のデモをナンバリング

---
+++ 1-incremental-search/app.js
@@ -0,0 +1,9 @@
+function MainCtrl($scope) {
+  $scope.people = [
+    { firstName: 'Kazuhito', lastName: 'Hokamura' },
+    { firstName: 'Takeshi',  lastName: 'Takatsudo' },
+    { firstName: 'Akihiro',  lastName: 'Oyamada' },
+    { firstName: 'Kazunori',  lastName: 'Tokuda' },
+    { firstName: 'Yukihisa',  lastName: 'Yamada' },
+  ];
+}
